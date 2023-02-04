dc911547c4f4d0a584cb1e1b14fdeb29dfa5324a
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2023-02-04 14:20:23 +0700

    Update definition.js

--- definition.js
+++ definition.js
@@ -3,7 +3,7 @@ Blockly.Blocks['tft_lcd_create'] = {
       this.jsonInit(
         {
             "type": "tft_lcd_create",
-            "message0": "khởi tạo màn hình TFT chân MISO %1MOSI %2SCK %3CS %4DC %5RST%6rộng %7 %8cao %9 %10 hướng %11%12",
+            "message0": "khởi tạo màn hình TFT chân MISO %1MOSI %2SCK %4CS %5DC %6RST%7rộng %8 %9cao %10 %11 hướng %12%13",
             "args0": [
               {
                 "type": "field_dropdown",
